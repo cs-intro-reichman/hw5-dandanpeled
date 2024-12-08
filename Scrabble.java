69a7213e13ab57cd19b245c2cbefced76bdc7edb
Dan Peled <mywebdan@gmail.com>
2024-12-08 21:36:39 +0200

    Scrabble - V3 changes in playhand print

--- Scrabble.java
+++ Scrabble.java
@@ -154,9 +154,9 @@ public class Scrabble {
 
 		}
 		if (hand.length() == 0) {
-	        System.out.println("Ran out of letters. Total score: " + score + " points");
+	        System.out.println("Ran out of letters. Total score: " + score + " points\n");
 		} else {
-			System.out.println("End of hand. Total score: " + score + " points \n");
+			System.out.println("End of hand. Total score: " + score + " points\n");
 		}
 	}
 
